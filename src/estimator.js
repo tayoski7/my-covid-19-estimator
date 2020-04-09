b7bc3d0ba36acd7f4974fe564dde2636a2b12c9c
Adetimehin James <tayoski7@gmail.com>
2020-04-09 20:13:24 +0100

    start building estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,10 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const input = data;
+    return {
+        data: input,
+        impact:{},
+        severalImpact:{}
+    };
+};
 
 export default covid19ImpactEstimator;
