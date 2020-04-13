adee37e901a2c916c4fcec6b7e5dfd5fee8b10ba
Adetimehin James <tayoski7@gmail.com>
2020-04-13 20:02:05 +0100

    building challenge

--- src/estimator.js
+++ src/estimator.js
@@ -1,10 +1,36 @@
 const covid19ImpactEstimator = (data) => {
-    const input = data;
-     return {
-        data: input,
-        impact: {},
-        severeImpact: {}
+    data={
+        region: {
+            name: "Africa",
+            avgAge: 19.7,
+            avgDailyIncomeInUSD: 5,
+            avgDailyIncomePopulation: 0.71,
+        };
+        periodType: "days",
+        timeToElapse: 58,
+        reportedCases: 674,
+        population: 66622705,
+        totalHospitalBeds: 1380614,
+        };
     };
-};
+
+//* challenge-1*//
+    return {
+        data: {},
+    
+        estimate:{
+            impact: { 
+                currentlyInfected: 6740,
+                infectionsByRequestedTime: 3450880,
+            },
+            severeImpact: {
+                currentlyInfected:33700,
+                infectionsByRequestedTime: 17254400,
+            };
+        };
+    };
+
+    //*challenge-2*//
+
 
 export default covid19ImpactEstimator;
